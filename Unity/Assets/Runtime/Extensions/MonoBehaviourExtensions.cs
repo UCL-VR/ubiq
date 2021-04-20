5bf351c95784fd47845449311b98b736d87fd6d4
Sebastian Friston <sebastian.friston@gmail.com>
2021-04-20 17:44:32 +0100

    Added Component extension to find components based on geodesic distance

---
+++ Unity/Assets/Runtime/Extensions/MonoBehaviourExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Ubiq.Extensions
+{
+    public static class MonoBehaviourExtensions
+    {
+        public static T GetClosestComponent<T>(this Component component) where T : MonoBehaviour
+        {
+            do
+            {
+                var behaviour = component.GetComponentInChildren<T>();
+                if (behaviour)
+                {
+                    return behaviour;
+                }
+                component = component.transform.parent;
+            } while (component != null);
+
+            return null;
+        }
+    }
+}
