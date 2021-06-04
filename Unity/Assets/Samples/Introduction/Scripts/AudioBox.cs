232053da665c6e8ca900d0095a6a27cae054c8cd
Sebastian Friston <sebastian.friston@gmail.com>
2021-06-04 11:55:51 +0100

    Removed Avatar Menu Box and replaced with Audio Box

---
+++ Unity/Assets/Samples/Introduction/Scripts/AudioBox.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Ubiq.XR;
+using UnityEngine;
+
+public class AudioBox : MonoBehaviour, IUseable, IGraspable
+{
+    private AudioSource audioSource;
+    private Hand follow;
+    private Rigidbody body;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        body = GetComponent<Rigidbody>();
+    }
+
+    public void Grasp(Hand controller)
+    {
+        follow = controller;
+    }
+
+    public void Release(Hand controller)
+    {
+        follow = null;
+    }
+
+    public void UnUse(Hand controller)
+    {
+
+    }
+
+    public void Use(Hand controller)
+    {
+        if (audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+        else
+        {
+            audioSource.Play();
+        }
+    }
+
+    private void Update()
+    {
+        if (follow != null)
+        {
+            transform.position = follow.transform.position;
+            transform.rotation = follow.transform.rotation;
+            body.isKinematic = true;
+        }
+        else
+        {
+            body.isKinematic = false;
+        }
+    }
+}
