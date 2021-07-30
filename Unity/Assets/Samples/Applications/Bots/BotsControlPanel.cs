30b58492c5e8c438887d2fbc23ecc6130afd2fd6
Sebastian Friston <sebastian.friston@gmail.com>
2021-07-30 14:21:32 +0100

    Updated Bots sample with manager component that can instantiate a number of bots on a real server

---
+++ Unity/Assets/Samples/Applications/Bots/BotsControlPanel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Ubiq.Samples.Bots;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BotsControlPanel : MonoBehaviour
+{
+    public Text NumberOfBotsLabel;
+    public Button CreateRoomButton;
+    public Button JoinRoomButton;
+    public Button AddBotsButton;
+    public Button ToggleCameraButton;
+    public InputField JoinCodeInputField;
+    public InputField NumberOfBotsInputField;
+
+    private BotsManager manager;
+
+    private void Awake()
+    {
+        manager = GetComponentInParent<BotsManager>();
+
+        CreateRoomButton.onClick.AddListener(() =>
+        {
+            manager.CreateRoom();
+        });
+
+        JoinRoomButton.onClick.AddListener(() =>
+        {
+            manager.JoinRoom();
+        });
+
+        AddBotsButton.onClick.AddListener(() =>
+        {
+            try
+            {
+                manager.AddBots(int.Parse(NumberOfBotsInputField.text));
+            }
+            catch(Exception e)
+            {
+                Debug.LogException(e);
+            }
+        });
+
+        ToggleCameraButton.onClick.AddListener(() =>
+        {
+            manager.ToggleCamera();
+        });
+
+        JoinCodeInputField.onValueChanged.AddListener(joinCode =>
+        {
+            manager.JoinCode = joinCode;
+        });
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NumberOfBotsLabel.text = manager.NumBots.ToString();
+        JoinCodeInputField.text = manager.JoinCode;
+    }
+}
---
+++ Unity/Assets/Samples/Applications/Bots/BotsManager.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Ubiq.Avatars;
+using Ubiq.Extensions;
+using Ubiq.Rooms;
+using UnityEngine;
+
+namespace Ubiq.Samples.Bots
+{
+    public class BotsManager : MonoBehaviour
+    {
+        public GameObject BotPeer;
+        public string JoinCode { get; set; }
+        public int NumBots { get => bots.Count; }
+
+        public Camera Camera;
+        private int cullingMask;
+
+        private string botManagerInstance;
+
+        /// <summary>
+        /// When True, Remote Avatars belonging to bots in this scene have their Mesh Renderers disabled.
+        /// </summary>
+        public bool HideBotAvatars = true;
+
+        private List<Bot> bots;
+
+        private void Awake()
+        {
+            botManagerInstance = Guid.NewGuid().ToString();
+            bots = new List<Bot>();
+            bots.AddRange(MonoBehaviourExtensions.GetComponentsInScene<Bot>());
+            cullingMask = Camera.cullingMask;
+        }
+
+        private void Start()
+        {
+            bots.ForEach(b => InitialiseBot(b));
+        }
+
+        /// <summary>
+        /// Create a new room and have all the bots join it. Any new bots will also join the room.
+        /// </summary>
+        public void CreateRoom()
+        {
+            if(bots.Count > 0)
+            {
+                var bot = bots.First();
+                var roomClient = GetRoomClient(bot);
+                roomClient.OnJoinedRoom.AddListener(room =>
+                {
+                    JoinCode = room.JoinCode;
+                    JoinRoom();
+                });
+                GetRoomClient(bot).JoinNew("Bots Room", false);
+            }
+        }
+
+        public void AddBot()
+        {
+            var newBot = GameObject.Instantiate(BotPeer);
+            var bot = newBot.GetComponentInChildren<Bot>();
+            bots.Add(bot);
+            InitialiseBot(bot);
+            JoinRoom(bot);
+        }
+
+        public void AddBots(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                AddBot();
+            }
+        }
+
+        public void JoinRoom()
+        {
+            foreach (var bot in bots)
+            {
+                JoinRoom(bot);
+            }
+        }
+
+        public void JoinRoom(Bot bot)
+        {
+            var rc = GetRoomClient(bot);
+            if(!string.IsNullOrEmpty(JoinCode) && rc.Room.JoinCode != JoinCode)
+            {
+                rc.Join(JoinCode);
+            }
+        }
+
+        private void InitialiseBot(Bot bot)
+        {
+            var rc = GetRoomClient(bot);
+            rc.Me["ubiq.botmanager.id"] = botManagerInstance;
+
+            var am = AvatarManager.Find(bot);
+            if(am)
+            {
+                am.OnAvatarCreated.AddListener(avatar =>
+                {
+                    if(HideBotAvatars)
+                    {
+                        if(avatar.Peer["ubiq.botmanager.id"] == botManagerInstance && !avatar.IsLocal)
+                        {
+                            foreach(var r in avatar.GetComponentsInChildren<MeshRenderer>())
+                            {
+                                r.enabled = false;
+                            }
+                            foreach (var r in avatar.GetComponentsInChildren<SkinnedMeshRenderer>())
+                            {
+                                r.enabled = false;
+                            }
+                        }
+                    }
+                });
+            }
+        }
+
+        private RoomClient GetRoomClient(Bot bot)
+        {
+            return bot.GetClosestComponent<RoomClient>();
+        }
+
+        public void ToggleCamera()
+        {
+            Camera.cullingMask ^= 1 << LayerMask.NameToLayer("Default");
+        }
+    }
+}
