14f642070b72496b58756a8dc7a3ec13bbc45e23
Klara <k.brandstatter@ucl.ac.uk>
2021-09-27 12:17:57 +0100

    puzzle piece fbx models, image catalogue for puzzle images, puzzle menu

---
+++ Unity/Assets/Samples/Applications/PuzzleTask/Scripts/ImageCatalogue.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Image Catalogue")]
+public class ImageCatalogue : ScriptableObject
+{
+    public List<Material> materials;
+
+    public Material GetRandomMaterial()
+    {
+        var i = Random.Range(0, materials.Count);
+        return materials[i];
+    }
+    public Material GetMaterial(string name)
+    {
+        foreach (var m in materials)
+        {
+            if (m.name == name)
+            {
+                return m;
+            }
+        }
+        return null;
+    }
+}
---
+++ Unity/Assets/Samples/Applications/PuzzleTask/Scripts/PuzzleMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleMenu : MonoBehaviour
+{
+    public Puzzle puzzle;
+    
+    public void SpawnPuzzle()
+    {
+        puzzle.SpawnPersistentPuzzle();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
