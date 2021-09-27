7282874bed68fcfa56f1731085eb0f0db4c67120
Klara <k.brandstatter@ucl.ac.uk>
2021-09-27 12:30:51 +0100

    set material for textured objects

--- Unity/Assets/Samples/Intro/Scripts/TexturedObject.cs
+++ Unity/Assets/Samples/Intro/Scripts/TexturedObject.cs
@@ -1,17 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using Ubiq.Messaging;
 
-public class TexturedObject : MonoBehaviour
+public class TexturedObject : MonoBehaviour, INetworkComponent
 {
+    public ImageCatalogue catalogue;
+    
+    private Material material;
     private Texture2D texture;
-    private GameObject quad;
     private new Renderer renderer;
+    private NetworkContext context;
+
+    public struct Message
+    {
+        public string matName;
+
+        public Message(string matName)
+        {
+            this.matName = matName;
+        }
+
+    }
+
+    public void SetMaterial(Material mat)
+    {
+        material = mat;
+        renderer.material = mat;
+    }
 
     public void SetTexture(Texture2D tex)
     {
         texture = tex;
         renderer.material.SetTexture("_MainTex", tex);
+    }
+
+    public Material GetMaterial()
+    {
+        return material;
     }
 
     public Texture2D GetTexture()
@@ -22,13 +49,28 @@ public class TexturedObject : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        quad = gameObject.transform.GetChild(0).gameObject;
-        renderer = quad.GetComponent<Renderer>();
+        renderer = GetComponentInChildren<Renderer>();
+        context = NetworkScene.Register(this);
+
+    }
+    void Start()
+    {
+        if (context == null)
+        {
+            context = NetworkScene.Register(this);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetNetworkedMaterial(Material mat)
     {
-        
+        SetMaterial(mat);
+        Debug.Log("Set networked material: " + mat.name);
+        context.SendJson(new Message(mat.name));
+    }
+
+    public void ProcessMessage(ReferenceCountedSceneGraphMessage message)
+    {
+        var msg = message.FromJson<Message>();
+        SetMaterial(catalogue.GetMaterial(msg.matName));
     }
 }
