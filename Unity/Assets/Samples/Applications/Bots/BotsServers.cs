36b2d4e4cf6113960b894fe7a9096822c780a979
Sebastian Friston <sebastian.friston@gmail.com>
2021-09-28 12:13:32 +0100

    Updated Bots sample hierarchy and added Component to set servers easily

---
+++ Unity/Assets/Samples/Applications/Bots/BotsServers.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Ubiq.Networking;
+using Ubiq.Rooms;
+using UnityEngine;
+
+namespace Ubiq.Samples.Bots
+{
+    public class BotsServers : MonoBehaviour
+    {
+        [Header("Command and Control Server")]
+        public ConnectionDefinition DefaultCommandServer;
+
+        [Header("Bots Server")]
+        public ConnectionDefinition DefaultBotServer;
+
+        private static BotsServers singleton;
+        private static BotsServers Singleton
+        {
+            get
+            {
+                if (singleton == null)
+                {
+                    // This may be called in any Awake method
+
+                    var scene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
+                    foreach (var item in scene.GetRootGameObjects())
+                    {
+                        singleton = item.GetComponentInChildren<BotsServers>();
+                        if (singleton)
+                        {
+                            break;
+                        }
+                    }
+                    if(singleton)
+                    {
+                        singleton.CheckCommandLineArguments();
+                    }
+                }
+                return singleton;
+            }
+        }
+
+        private void CheckCommandLineArguments()
+        {
+            // todo
+        }
+
+        public static ConnectionDefinition CommandServer
+        {
+            get
+            {
+                return Singleton.DefaultCommandServer;
+            }
+        }
+
+        public static ConnectionDefinition BotServer
+        {
+            get
+            {
+                return Singleton.DefaultBotServer;
+            }
+        }
+    }
+}
