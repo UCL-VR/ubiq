6f14102cc55e9b7791c8497a7d7aef60d4d3afcf
Klara <k.brandstatter@ucl.ac.uk>
2021-10-04 17:49:13 +0100

    add tomato presence tFloatingBodyAvatar, fixed physics in PuzzlePiece

--- Unity/Assets/Samples/Applications/PuzzleTask/Scripts/PuzzlePiece.cs
+++ Unity/Assets/Samples/Applications/PuzzleTask/Scripts/PuzzlePiece.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using Ubiq.Messaging;
 using Ubiq.Spawning;
 using Ubiq.XR;
+using Ubiq.Rooms;
+
 
 /// <summary>
 /// A puzzle piece belongs to a Puzzle GameObject in the scene. It is spawned by the NetworkSpawner in the Puzzle.
@@ -27,10 +29,13 @@ public class PuzzlePiece : MonoBehaviour, INetworkObject, INetworkComponent, ISp
     private bool isKinematic = false;
     private Vector3 previousPosition = Vector3.zero;
 
+    private bool initPosForRecording = false;
+
     public NetworkId Id { get; set; } // the network Id will be set by the spawner, which will always be the one to instantiate the PuzzlePiece
 
     private NetworkContext context;
     private NetworkScene scene;
+    private RoomClient roomClient;
 
     public struct Message
     {
@@ -71,6 +76,7 @@ public class PuzzlePiece : MonoBehaviour, INetworkObject, INetworkComponent, ISp
         // needs to be here because we want to network messages immediately
         context = NetworkScene.Register(this);
         scene = NetworkScene.FindNetworkScene(this);
+        roomClient = scene.gameObject.GetComponent<RoomClient>();
         
     }
 
@@ -99,6 +105,7 @@ public class PuzzlePiece : MonoBehaviour, INetworkObject, INetworkComponent, ISp
     {
         follow = null;
         owner = owned = false;
+        context.SendJson(new Message(transform, false, true));
     }
 
     public bool IsLocal()
@@ -126,6 +133,7 @@ public class PuzzlePiece : MonoBehaviour, INetworkObject, INetworkComponent, ISp
         transform.localPosition = trafo.localPosition;
         transform.localRotation = trafo.localRotation;
         context.SendJson(new Message(trafo, owner, true)); // isKinematic is true
+        //Debug.Log("Set networked transform: " + trafo.localPosition.ToString());
 
     }
 
@@ -137,13 +145,15 @@ public class PuzzlePiece : MonoBehaviour, INetworkObject, INetworkComponent, ISp
         owned = msg.owned;
         isKinematic = msg.isKinematic;
         body.isKinematic = msg.isKinematic;
+        //Debug.Log("Set networked transform remote: " + msg.transform.position.ToString());
+
     }
 
     // Update is called once per frame
     // physics is making things unpredictable...
     void Update()
     {
-        // if someone is holding the puzzle piece the transform is controlled by the grabber and not physics
+        // if someone is holding the puzzle piece the transform is controlled by the grabber and not physics (no matter who owns it, local or remote)
         if (follow != null) // means this is owner too
         {
             transform.position = follow.TransformPoint(localGrabPoint);
@@ -163,22 +173,30 @@ public class PuzzlePiece : MonoBehaviour, INetworkObject, INetworkComponent, ISp
             }
             else // unless nobody owns the piece
             {
-                body.isKinematic = false;
-               
-                // then we need to check if we are recording and also send messages accordingly
-                if (local && scene.recorder != null && scene.recorder.IsRecording())
-                { 
+                // then the piece should be controlled by the physics of the creator of the room
+                if (roomClient.Me["creator"] == "1") // or local??? but what if local peer goes away?
+                {
+                    body.isKinematic = false;
                     // only send new message if the position has changed
                     if (!transform.position.Equals(previousPosition))
                     {
                         context.SendJson(new Message(transform, owned, true));
-                        Debug.Log("Transforms " + previousPosition.ToString() + " " + transform.position.ToString());
+                        //Debug.Log("Transforms " + previousPosition.ToString() + " " + transform.position.ToString());
                         previousPosition = transform.position;
                     }
-                }
-                else
-                {
-                    previousPosition = Vector3.zero; // when recording is stopped reset previousPosition for potential new recording
+                    // then we need to check if we are recording and also send messages accordingly
+                    else if (scene.recorder != null && scene.recorder.IsRecording())
+                    {
+                        if (!initPosForRecording)
+                        {
+                            previousPosition = Vector3.zero; // when recording is stopped reset previousPosition for potential new recording
+                            initPosForRecording = true;
+                        }
+                    }
+                    else
+                    {
+                        initPosForRecording = false;
+                    }
                 }
             }
             //Debug.Log("Transforms " + gameObject.name + " " + transform.position.ToString());
