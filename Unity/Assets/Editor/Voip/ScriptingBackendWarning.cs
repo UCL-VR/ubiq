32a8e1faa91c8e80861fc1310c44c0e96414b8fb
bnjco <33021110+bnjco@users.noreply.github.com>
2021-10-27 16:54:50 +0100

    Fix issue where permissions were expected too early, causing microphone to fail on first run on Quest platforms, add warning for Mono backend which causes slow voip connection init

---
+++ Unity/Assets/Editor/Voip/ScriptingBackendWarning.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+
+namespace Ubiq.Voip
+{
+    // Warn the user if they're using Mono rather than IL2CPP as their scripting
+    // backend. The Mono backend takes a long time to generate certificates
+    // when used with our VOIP library on Android.
+    public static class ScriptingBackendWarning
+    {
+        [UnityEditor.Callbacks.DidReloadScripts]
+        private static void OnScriptsReloaded() {
+#if !UBIQ_SILENCEWARNING_SCRIPTINGBACKEND
+            var buildTarget = EditorUserBuildSettings.activeBuildTarget;
+            if (buildTarget != BuildTarget.Android)
+            {
+                return;
+            }
+
+            var buildTargetGroup = BuildPipeline.GetBuildTargetGroup(buildTarget);
+            var backend = PlayerSettings.GetScriptingBackend(buildTargetGroup);
+            if (backend == ScriptingImplementation.IL2CPP)
+            {
+                return;
+            }
+
+            UnityEngine.Debug.LogWarning(
+                "IL2CPP is the preferred scripting backend for Ubiq on Android." +
+                " VOIP connections will take significantly longer to establish" +
+                " when using Mono. To silence this warning, add the string " +
+                " UBIQ_SILENCEWARNING_SCRIPTINGBACKEND to your scripting define symbols");
+#endif
+        }
+    }
+}
--- Unity/Assets/Runtime/Voip/VoipMicrophoneInput.cs
+++ Unity/Assets/Runtime/Voip/VoipMicrophoneInput.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using SIPSorceryMedia.Abstractions;
 using UnityEngine;
 
-#if UNITY_ANDROID
+#if UNITY_ANDROID && !UNITY_EDITOR
 using UnityEngine.Android;
 #endif
 
@@ -143,9 +143,13 @@ namespace Ubiq.Voip
         private TaskCompletionSource<bool> allTaskTcs = new TaskCompletionSource<bool>();
         private readonly object taskLock = new object();
 
+#if UNITY_ANDROID && !UNITY_EDITOR
+        private bool microphoneAuthorized = false;
+#endif
+
         private void Awake()
         {
-#if UNITY_ANDROID
+#if UNITY_ANDROID && !UNITY_EDITOR
             if (!Permission.HasUserAuthorizedPermission(Permission.Microphone))
             {
                 Permission.RequestUserPermission(Permission.Microphone);
@@ -168,6 +172,20 @@ namespace Ubiq.Voip
 
         private void Update()
         {
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+            // Wait for microphone permissions before processing any audio
+            if (!microphoneAuthorized)
+            {
+                microphoneAuthorized = Permission.HasUserAuthorizedPermission(Permission.Microphone);
+
+                if (!microphoneAuthorized)
+                {
+                    return;
+                }
+            }
+#endif
+
             // Run queued tasks synchronously
             while (true)
             {
