6420899279112976972e4b2f380417883dbe0a1c
bnjco <33021110+bnjco@users.noreply.github.com>
2022-01-12 15:07:50 +0000

    Add optional flying controls to player

--- Unity/Assets/Runtime/XR/XRPlayerController.cs
+++ Unity/Assets/Runtime/XR/XRPlayerController.cs
@@ -19,6 +19,9 @@ namespace Ubiq.XR
         private Vector3 velocity;
         private Vector3 userLocalPosition;
 
+        public float joystickDeadzone = 0.1f;
+        public float joystickFlySpeed = 1.2f;
+
         public Camera headCamera;
         public Transform cameraContainer;
         public AnimationCurve cameraRubberBand;
@@ -89,25 +92,41 @@ namespace Ubiq.XR
         }
 
 
-        private void FixedUpdate()
+        private void Update()
         {
             // Update the foot position. This is done by pulling the feet using a rubber band.
             // Decoupling the feet in this way allows the user to do things like lean over edges, when the ground check is enabled.
             // This can be effectively disabled by setting the animation curve to a constant high value.
+
+            foreach (var item in handControllers)
+            {
+                if (item.Right)
+                {
+                    if (item.JoystickSwipe.Trigger)
+                    {
+                        transform.RotateAround(headCamera.transform.position, Vector3.up, 45f * Mathf.Sign(item.JoystickSwipe.Value));
+                    }
+                }
+                else if (item.Left)
+                {
+                    var dir = item.Joystick.normalized;
+                    var mag = item.Joystick.magnitude;
+                    if (mag > joystickDeadzone)
+                    {
+                        var speedMultiplier = Mathf.InverseLerp(joystickDeadzone,1.0f,mag);
+                        var worldDir = headCamera.transform.TransformDirection(dir.x,0,dir.y);
+                        worldDir.y = 0;
+                        var distance = (joystickFlySpeed * Time.deltaTime);
+                        transform.position += distance * worldDir.normalized;
+                    }
+                }
+            }
 
             var headProjectionXZ = transform.InverseTransformPoint(headCamera.transform.position);
             headProjectionXZ.y = 0;
             userLocalPosition.x += (headProjectionXZ.x - userLocalPosition.x) * Time.deltaTime * cameraRubberBand.Evaluate(Mathf.Abs(headProjectionXZ.x - userLocalPosition.x));
             userLocalPosition.z += (headProjectionXZ.z - userLocalPosition.z) * Time.deltaTime * cameraRubberBand.Evaluate(Mathf.Abs(headProjectionXZ.z - userLocalPosition.z));
             userLocalPosition.y = 0;
-
-            foreach (var item in handControllers)
-            {
-                if(item.JoystickSwipe.Trigger)
-                {
-                    transform.RotateAround(headCamera.transform.position, Vector3.up, 45f * Mathf.Sign(item.JoystickSwipe.Value));
-                }
-            }
         }
 
         private void OnDrawGizmos()
