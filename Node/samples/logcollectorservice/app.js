54221a0989f269af49729a548a442198c41a0c02
Sebastian Friston <sebastian.friston@gmail.com>
2023-01-18 13:33:09 +0000

    Put nexus back as default host in LogCollector Sample

--- Node/samples/logcollectorservice/app.js
+++ Node/samples/logcollectorservice/app.js
@@ -6,7 +6,7 @@
 // add a RoomClient and LogCollector component(s). These join a room and recieve 
 // log messages.
 // 
-// The LogCollector uses the Id of the peers to decide where to write the events. 
+// The LogCollector uses the Id of the peers to decide where to write the events.
 // It creates new files on demand, and closes them when the corresponding Peer 
 // has left the room.
 
@@ -21,7 +21,7 @@ eventType = 2;
 roomGuid = "6765c52b-3ad6-4fb0-9030-2c9a05dc4731";
 
 // Create a connection to a Server
-const connection = UbiqTcpConnection("localhost", 8009);
+const connection = UbiqTcpConnection("nexus.cs.ucl.ac.uk", 8009);
 
 // A NetworkScene
 const scene = new NetworkScene();
@@ -31,7 +31,8 @@ scene.addConnection(connection);
 const roomclient = new RoomClient(scene);
 const logcollector = new LogCollector(scene);
 
-// A list of open files to write events for particular peers into (we can close these when the peers leave the room)
+// A list of open files to write events for particular peers into (we can close
+// these when the peers leave the room)
 const files = {};
 
 function writeEventToPeerFile(peer, message){
@@ -54,7 +55,7 @@ roomclient.addListener("OnJoinedRoom", room => {
 });
 
 roomclient.addListener("OnPeerAdded", peer =>{
-    console.log("New Peer in room " + peer.uuid);
+    console.log("New Peer " + peer.uuid + " joined Room");
 });
 
 roomclient.addListener("OnPeerRemoved", peer =>{
@@ -69,10 +70,14 @@ logcollector.addListener("OnLogMessage", (type,message) => {
     }
 });
 
-// Calling startCollection()/lockCollection() will start streaming from the LogManagers at existing and
-// and new Peers. Call this before joining a new room.
-// lockCollection() is like startCollection(), but the Collector will automatically maintain its status
-// as the primary collector for as long as it runs.
+// Calling startCollection()/lockCollection() will start streaming from the 
+// LogManagers at existing and and new Peers. Call this before joining a new 
+// room.
+// lockCollection() is like startCollection(), but the Collector will 
+// automatically maintain its status as the primary collector for as long as it
+// runs.
 logcollector.lockCollection();
 
-roomclient.join(roomGuid); // Join by UUID. Use an online generator to create a new one for your experiment.
+// Join by UUID. Use an online generator to create a new one for your 
+// experiment.
+roomclient.join(roomGuid); 
